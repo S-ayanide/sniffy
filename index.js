d8c818055f819c0300e2a606d82d0cc98c276e4c
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-16 21:54:05 +0530

    Added push and edit issue rule
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- index.js
+++ index.js
@@ -174,11 +174,26 @@ module.exports = app => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-    const params = context.issue({ body: 'Hello World!' })
+    const { body } = context.payload.issue;
+    const params = context.issue({ body: body.includes('Test') ? 'You just wrote Test' : 'Random Bot Comment'})
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
   })
+
+  app.on(['issues.opened', 'issues.edited'], async context => {
+    // An issue was opened or edited, what should we do with it?
+    app.log(context)
+    
+    const params = context.issue({ body: 'You have edited the Issue'})
+    return context.github.issues.createComment(params)
+  })
+
+  app.on('push', async context => {
+    // Code was pushed to the repo, what should we do with it?
+    app.log(context)
+  })
+  
 
   // we don't care about marketplace events
   app.on('marketplace_purchase', async context => {
